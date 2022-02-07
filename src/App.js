14efc5b69e25d1701ddc97376a924a63054c9afa
deboracaires <deboracaires@pop-os.localdomain>
2022-02-07 09:58:20 -0300

    validacoes colocadas na signIn page, userContext criado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 import SignInPage from './components/signIn/SignInPage.js';
+import UserContext from './contexts/UserContext.js';
 
 function App() {
-    
+  const [user, setUser] = useState();
+
   return (
     <BrowserRouter>
-      <Routes>
-      <Route path='/' element={<SignInPage/>} exact/>
-      </Routes>
+      <UserContext.Provider value={{user, setUser}}>
+        <Routes>
+          <Route path='/' element={<SignInPage/>} exact/>
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- src/components/signIn/SignInPage.js
+++ src/components/signIn/SignInPage.js
@@ -1,18 +1,50 @@
-import { useState } from "react";
-import { postSignIn } from "../../services/api.services";
-import { Conteiner, SignInTitle, Input, Button, SignUpText } from "../../styles/SignInAndSignUpStyles";
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router';
+import Swal from 'sweetalert2';
+import UserContext from '../../contexts/UserContext.js';
+import { postSignIn } from '../../services/api.services.js';
+import { Conteiner, SignInTitle, Input, Button, SignUpText } from '../../styles/SignInAndSignUpStyles.js';
+import { signInValidate } from '../../validations/signInValidation.js';
 
 export default function SignInPage () {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   
+  const navigate = useNavigate();
+
+  const { setUser } = useContext(UserContext);
+
   function doSignIn(e) {
     e.preventDefault();
     const body = { email, password};
-    
-    postSignIn(body)
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+    if (signInValidate(email, password)) {
+      postSignIn(body)
+      .then((res) => {
+        setUser(res.data);
+        const token = JSON.stringify(res.data.token);
+        sessionStorage.setItem("token", token);
+        const name = JSON.stringify(res.data.name);
+        sessionStorage.setItem("name", name);
+        navigate('/principal');
+      })
+      .catch((err) => {
+        if (err.response.status === 401){ 
+          Swal.fire({
+            html: `<h1 style = 'color: #fff'>E-mail e/ou senha incorretos!</h1>`,
+            width: '95%',
+            background: '#8C11BE',
+            confirmButtonColor: '#A328D6',
+          });
+        } else if (err.response.status === 422){ 
+          Swal.fire({
+            html: `<h1 style = 'color: #fff'>Algum dado está inválido, tente novamente!</h1>`,
+            width: '95%',
+            background: '#8C11BE',
+            confirmButtonColor: '#A328D6',
+          });
+       }
+      });
+    }
   }
 
   return (
@@ -23,7 +55,7 @@ export default function SignInPage () {
         <Input placeholder="Senha" type="password"value={password} onChange={e => setPassword(e.target.value)}></Input>
         <Button type="submit">Entrar</Button>
       </form>
-      <SignUpText>Primeira vez? Cadastre-se!</SignUpText>
+      <SignUpText onClick = {() => navigate('/sign-up')}>Primeira vez? Cadastre-se!</SignUpText>
     </Conteiner>
   );
 }
